f90c645f9f89a656143c07210299025d17d407ee
wachukxs <nwachukwuossai@gmail.com>
2019-12-21 17:06:22 +0100

    know who posts what

--- ards_markettrends.sql
+++ ards_markettrends.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 28, 2019 at 02:38 PM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Host: 127.0.0.1
+-- Generation Time: Dec 21, 2019 at 05:04 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,24 +28,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `marketmonitoring`
 --
 
-DROP TABLE IF EXISTS `marketmonitoring`;
-CREATE TABLE IF NOT EXISTS `marketmonitoring` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `marketmonitoring` (
+  `id` int(255) NOT NULL,
   `date` date NOT NULL,
   `marketname` varchar(50) NOT NULL,
   `producename` varchar(50) NOT NULL,
   `price` int(255) NOT NULL,
   `quantity` int(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `username` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `marketmonitoring`
 --
 
-INSERT INTO `marketmonitoring` (`id`, `date`, `marketname`, `producename`, `price`, `quantity`) VALUES
-(1, '2009-11-12', 'ORganized360', 'aaa', 200, 20),
-(2, '2009-11-12', 'Abaji', 'Rice', 2000, 20);
+INSERT INTO `marketmonitoring` (`id`, `date`, `marketname`, `producename`, `price`, `quantity`, `username`) VALUES
+(1, '2009-11-12', 'ORganized360', 'aaa', 200, 20, ''),
+(2, '2009-11-12', 'Abaji', 'Rice', 2000, 20, ''),
+(3, '2019-12-26', 'Something', 'Another thing', 3343, 76, 'remiadebayo');
 
 -- --------------------------------------------------------
 
@@ -53,17 +53,15 @@ INSERT INTO `marketmonitoring` (`id`, `date`, `marketname`, `producename`, `pric
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(20) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
   `password` varchar(128) NOT NULL,
-  `position` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `position` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -78,6 +76,38 @@ INSERT INTO `user` (`id`, `email`, `username`, `firstname`, `lastname`, `passwor
 (6, 'sadeadu@gmail.com', 'itsrheezy', 'Sade', 'Adebayo', '0000', 'ProgramAnalyst'),
 (7, 'remmyghty@gmail.com', 'FEMI_', 'Oluremi', 'Adebayo', '1111', 'ProgramAnalyst'),
 (8, 'remmyghty@gmail.com', 'wale', 'Oluremi', 'Adebayo', '12345', 'ProgramAnalyst');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `marketmonitoring`
+--
+ALTER TABLE `marketmonitoring`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `marketmonitoring`
+--
+ALTER TABLE `marketmonitoring`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
